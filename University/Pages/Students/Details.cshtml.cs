b89f2ab3f646ff3a50d916c52f192378bf4c4429
dragonrossa <rodjuga@gmail.com>
2020-12-17 11:14:07 +0100

    Students

---
+++ University/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+using University.Models;
+
+namespace University.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly University.Data.UniversityContext _context;
+
+        public DetailsModel(University.Data.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
