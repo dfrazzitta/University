cfd50b4e8ece98f82175de0fdf8e34ac71825866
dragonrossa <rodjuga@gmail.com>
2020-12-18 10:56:04 +0100

    Sorting, filtering, paging, grouping

--- University/Pages/Students/Index.cshtml.cs
+++ University/Pages/Students/Index.cshtml.cs
@@ -10,48 +10,66 @@ using University.Models;
 
 namespace University.Pages.Students
 {
-    public class IndexModel : PageModel
-    {
-        private readonly UniversityContext _context;
-        public IndexModel(UniversityContext context)
+        public class IndexModel : PageModel
         {
-            _context = context;
-        }
+            private readonly UniversityContext _context;
 
-        public string NameSort { get; set; }
-        public string DateSort { get; set; }
-        public string CurrentFilter { get; set; }
-        public string CurrentSort { get; set; }
-
-        public IList<Student> Students { get; set; }
-
-        public async Task OnGetAsync(string sortOrder)
-        {
-            // using System;
-            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-
-            IQueryable<Student> studentsIQ = from s in _context.Students
-                                             select s;
-
-            switch (sortOrder)
+            public IndexModel(UniversityContext context)
             {
-                case "name_desc":
-                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
-                    break;
-                case "Date":
-                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
-                    break;
-                case "date_desc":
-                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
-                    break;
-                default:
-                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
-                    break;
+                _context = context;
             }
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            public string NameSort { get; set; }
+            public string DateSort { get; set; }
+            public string CurrentFilter { get; set; }
+            public string CurrentSort { get; set; }
+
+            public PaginatedList<Student> Students { get; set; }
+
+            public async Task OnGetAsync(string sortOrder,
+                string currentFilter, string searchString, int? pageIndex)
+            {
+                CurrentSort = sortOrder;
+                NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+                if (searchString != null)
+                {
+                    pageIndex = 1;
+                }
+                else
+                {
+                    searchString = currentFilter;
+                }
+
+                CurrentFilter = searchString;
+
+                IQueryable<Student> studentsIQ = from s in _context.Students
+                                                 select s;
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                           || s.FirstMidName.Contains(searchString));
+                }
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                        break;
+                    case "Date":
+                        studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                        break;
+                    case "date_desc":
+                        studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                        break;
+                    default:
+                        studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                        break;
+                }
+
+                int pageSize = 3;
+                Students = await PaginatedList<Student>.CreateAsync(
+                    studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+            }
         }
-    }
 
 }
