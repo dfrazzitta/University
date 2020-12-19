f759bd1053e87ea999dcca954814fbee005a2aa3
dragonrossa <rodjuga@gmail.com>
2020-12-19 20:28:38 +0100

    Department and rowversions

---
+++ University/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+using University.Models;
+
+namespace University.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly University.Data.UniversityContext _context;
+
+        public IndexModel(University.Data.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
